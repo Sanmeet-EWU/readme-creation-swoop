010ec4e61008a6ba0be05e635c20f152f6613f16
Jacob Lucas <jlucas18@ewu.edu>
2024-08-08 14:26:11 -0700

    Unit Tests Integration

---
+++ SwoopHealth/Subviews/Patients/PatientItem.swift
@@ -0,0 +1,18 @@
+//
+//  PatientItem.swift
+//  SwoopHealth
+//
+//  Created by Jacob Lucas on 8/7/24.
+//
+
+import SwiftUI
+
+struct PatientItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PatientItem()
+}
